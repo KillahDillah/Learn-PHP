b40065186c32d58dbd190469b1f195e084144d3b
Leah <leah.miller@processmaker.com>
2018-12-16 19:37:03 -0800

    get key value pair from URL

--- first/index.php
+++ first/index.php
@@ -1,5 +1,24 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Learnin' PHP</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<style>
 
-$greeting = "Welcome";
+</style>
 
-echo $greeting;
+</head>
+<body>
+    <header class="bg-light text-center">
+        <h1>Hello
+            <?php
+            $name = $_GET["name"];
+            echo $name;
+            ?>
+        </h1>
+
+    </header>
+    
+</body>
+</html>
