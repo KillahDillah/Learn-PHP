ea37a1d4a60edcf40d08206933b905409b603a0e
Leah <leah.miller@processmaker.com>
2018-12-16 22:38:59 -0800

    foreach loop and dynamic data

--- first/home.php
+++ first/home.php
@@ -4,6 +4,7 @@
 <meta charset="UTF-8">
 <title>Learnin' PHP</title>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<link href="app.css" rel="stylesheet" type="text/css" />
 <style>
 
 </style>
@@ -14,10 +15,18 @@
         <h1>
             <?= $greeting ?>
             <br>
-            <?= $age , $gender?>
+            <?= $age, $gender ?>
         </h1>
-
     </header>
+    <div class="bg-gray">
+        <ul class="list-unstyled d-flex text-center justify-content-between">
+        <?php foreach ($familyMembers as $member){
+                echo "<li>$member</li>";
+            }
+        ?>
+        </ul>
+        
+    </div>
     
 </body>
 </html>
--- first/index.php
+++ first/index.php
@@ -3,6 +3,20 @@
 $greeting = "Hello, " . htmlspecialchars($_GET["name"]);
 $age = "You are a " . htmlspecialchars($_GET["age"]) . " year old ";
 $gender = htmlspecialchars($_GET["gender"]);
+$name = " are family";
+
+$familyMembers = [
+    "Leah",
+    "Audra",
+    "Lynda",
+    "Rex",
+    "Ryan",
+    "Russell",
+    "Rowen",
+    "Barkley",
+    "Jack",
+    "Cat"
+];
+
 
 require 'home.php';
-require 'contact.php';
